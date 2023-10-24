5fd9de491dd54a8c26939297cf11fbb3fb97c15c
Aliaksandr <dyvedd@gmail.com>
2023-10-24 16:36:20 +0300

    add logic

--- app/src/main/java/com/example/sumincalculator/MainActivity.java
+++ app/src/main/java/com/example/sumincalculator/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.sumincalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		EditText editTextAnswer = findViewById(R.id.editTextAnswer);
+		Button buttonAnswer = findViewById(R.id.buttonAnswer);
+		TextView textViewWrongAnswer = findViewById(R.id.textViewWrongAnswer);
+		TextView textViewRightAnswer = findViewById(R.id.textViewRightAnswer);
+
+		buttonAnswer.setOnClickListener(v -> {
+
+			String textAnswer = editTextAnswer.getText().toString();
+			if (textAnswer.equals("15")) {
+				textViewRightAnswer.setVisibility(View.VISIBLE);
+				textViewWrongAnswer.setVisibility(View.GONE);
+			} else {
+				textViewRightAnswer.setVisibility(View.GONE);
+				textViewWrongAnswer.setVisibility(View.VISIBLE);
+			}
+
+		});
 	}
 }
